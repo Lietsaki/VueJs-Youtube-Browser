abaaa345df6a52a01d945425aa1b3845ac823bdb
Ricardo Sandez <ricardosandez67@gmail.com>
2020-03-11 22:08:37 -0500

    fixed comment position in main.js

--- src/main.js
+++ src/main.js
@@ -4,11 +4,11 @@ import App from './App.vue';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// Inside our render function, we'll return the createElement function, but the convention is to call it 'h', so we'll use that here.
+//   render: function(h) {
+//     return h(App);
+//   }
+// Also, instead of this classic version, we use an arrow function like you see down there, which is 100% equivalent.
 new Vue({
-  // Inside our render function, we'll return the createElement function, but the convention is to call it 'h', so we'll use that here.
-  //   render: function(h) {
-  //     return h(App);
-  //   }
-  // Also, instead of this classic version, we use an arrow function like you see down there, which is 100% equivalent.
   render: h => h(App)
 }).$mount('#app');
